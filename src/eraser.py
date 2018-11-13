190b3c84e3856fc656ec95165c472c6e5801d1a7
jmoses45 <jmoses45@yahoo.com>
2018-11-13 13:40:39 -0600

    Added erase text function to earser and accompanying tests.

--- src/eraser.py
+++ src/eraser.py
@@ -5,3 +5,22 @@ class Eraser:
 
     def set_durability(self, amount):
         self.current_durability = amount
+
+    def erase(self, paper, text_to_erase):
+        text_cost = 0
+        old_text = ""
+        new_text = ""
+
+        for c in text_to_erase[::-1]:
+            if c.isspace():
+                old_text += c
+                new_text += " "
+            else:
+                text_cost += 1
+                if self.current_durability >= text_cost:
+                    old_text += c
+                    new_text += " "
+
+        self.current_durability -= text_cost
+
+        paper.text = paper.text[::-1].replace(old_text, new_text, 1)[::-1]
--- test/test_eraser.py
+++ test/test_eraser.py
@@ -1,3 +1,4 @@
+from src.paper import Paper
 from test.test_pencil import TestPencil
 
 
@@ -6,3 +7,57 @@ class AttributeTestEraser(TestPencil):
     def test_when_eraser_set_durability_is_passed_a_number_it_sets_the_current_durability_variable_in_the_pencil_eraser_instance(self):
         self.pencil.eraser.set_durability(1000)
         self.assertEqual(self.pencil.eraser.current_durability, 1000)
+
+    def test_when_eraser_is_given_a_string_to_erase_it_losses_durability_matching_amount_erased(self):
+        self.pencil.eraser.set_durability(1000)
+
+        paper = Paper()
+        paper.text = "Hello World!"
+
+        self.pencil.eraser.erase(paper, "World")
+
+        self.assertEqual(self.pencil.eraser.current_durability, 995)
+
+    def test_when_eraser_is_given_a_string_with_spaces_to_erase_it_losses_durability_matching_amount_erased(self):
+        self.pencil.eraser.set_durability(1000)
+
+        paper = Paper()
+        paper.text = "Hello World!"
+
+        self.pencil.eraser.erase(paper, " World")
+
+        self.assertEqual(self.pencil.eraser.current_durability, 995)
+
+    def test_when_eraser_is_given_a_string_to_erase_and_has_enough_durability_to_do_so(self):
+        self.pencil.eraser.set_durability(1000)
+
+        paper = Paper()
+        paper.text = "Hello World!"
+
+        self.pencil.eraser.erase(paper, "World")
+
+        self.assertEqual(paper.text, "Hello      !")
+
+    def test_when_eraser_is_given_a_string_to_erase_and_does_not_have_enough_durability_to_do_so(self):
+        self.pencil.eraser.set_durability(3)
+
+        paper = Paper()
+        paper.text = "Hello World!"
+
+        self.pencil.eraser.erase(paper, "World")
+
+        self.assertEqual(paper.text, "Hello Wo   !")
+
+    def test_when_eraser_is_given_a_complex_string_to_erase_multiple_parts_of(self):
+        self.pencil.eraser.set_durability(1000)
+
+        paper = Paper()
+        paper.text = "How much wood would a woodchuck chuck if a woodchuck could chuck wood?"
+
+        self.pencil.eraser.erase(paper, "chuck")
+
+        self.assertEqual(paper.text, "How much wood would a woodchuck chuck if a woodchuck could       wood?")
+
+        self.pencil.eraser.erase(paper, "woodchuck")
+
+        self.assertEqual(paper.text, "How much wood would a woodchuck chuck if a           could       wood?")
