98c57a33baeeef916a53de957d6fdefe1de9210a
jmoses45 <jmoses45@yahoo.com>
2018-11-13 02:05:41 -0600

    Added test function to add text to existing text.

--- test/test_paper.py
+++ test/test_paper.py
@@ -7,14 +7,20 @@ from src.paper import Paper
 
 class TestPaper(TestCase):
 
-    def test_when_write_is_passed_string_it_adds_string_to_text_variable_in_the_paper_instance(self):
-        paper = Paper()
-        paper.write("Hello World!")
-        self.assertEqual(paper.text, "Hello World!")
-
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_when_print_text_is_called_it_prints_text_variable_to_stdout(self, mock_output):
         paper = Paper()
         paper.write("Hello World!")
         paper.print_text()
         self.assertEqual(mock_output.getvalue(), "Hello World!\n")
+
+    def test_when_write_is_passed_string_it_adds_string_to_text_variable_in_the_paper_instance(self):
+        paper = Paper()
+        paper.write("Hello World!")
+        self.assertEqual(paper.text, "Hello World!")
+
+    def test_when_write_is_passed_string_it_adds_string_to_text_variable_in_the_paper_instance_with_existing_text(self):
+        paper = Paper()
+        paper.text = "Hello "
+        paper.write("World!")
+        self.assertEqual(paper.text, "Hello World!")
