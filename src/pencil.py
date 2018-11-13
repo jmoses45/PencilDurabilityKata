bff4a45a329c200d94c99347ba0e40816f0698cc
jmoses45 <jmoses45@yahoo.com>
2018-11-12 22:57:20 -0600

    Added length to pencil class, added funciton to set length, and changed organization of pencil test classes.

--- src/pencil.py
+++ src/pencil.py
@@ -1,11 +1,17 @@
 class Pencil:
 
     def __init__(self):
-        self.current_tip_durability = 0;
+        self.current_length = 0
+        self.current_tip_durability = 0
         self.max_tip_durability = 0
+
+    def set_length(self, length):
+        self.current_length = length
 
     def set_max_tip_durability(self, amount):
         self.max_tip_durability = amount
 
     def sharpen(self):
-        self.current_tip_durability = self.max_tip_durability;
+        if self.current_length > 0:
+            self.current_length -= 1
+            self.current_tip_durability = self.max_tip_durability
--- test/test_pencil.py
+++ test/test_pencil.py
@@ -3,17 +3,34 @@ from src.pencil import Pencil
 
 
 class TestPencil(TestCase):
+    def setUp(self):
+        self.pencil = Pencil()
+
+    def tearDown(self):
+        self.pencil = None
+
+
+class AttributeTestPencil(TestPencil):
+    def test_when_set_length_is_passed_a_number_it_sets_the_current_length_variable_in_the_pencil_instance(self):
+        self.pencil.set_length(10)
+        self.assertEqual(self.pencil.current_length, 10)
+
     def test_when_set_max_tip_durability_is_passed_a_number_it_sets_the_max_tip_durability_variable_in_the_pencil_instance(self):
-        pencil = Pencil()
-        pencil.set_max_tip_durability(1000)
-        self.assertEqual(pencil.max_tip_durability, 1000)
+        self.pencil.set_max_tip_durability(1000)
+        self.assertEqual(self.pencil.max_tip_durability, 1000)
+
+
+class LongTestPencil(TestPencil):
+    def setUp(self):
+        super(LongTestPencil, self).setUp()
+        self.pencil.set_length(10)
+        self.pencil.set_max_tip_durability(1000)
 
     def test_when_sharpen_is_called_it_sets_the_current_tip_durability_variable_in_the_pencil_instance_to_max_tip_durability(self):
-        pencil = Pencil()
-        pencil.set_max_tip_durability(1000)
+        self.assertEqual(self.pencil.current_tip_durability, 0)
 
-        self.assertEqual(pencil.current_tip_durability, 0)
+        self.pencil.sharpen()
 
-        pencil.sharpen()
+        self.assertEqual(self.pencil.current_tip_durability, 1000)
 
-        self.assertEqual(pencil.current_tip_durability, 1000)
+
